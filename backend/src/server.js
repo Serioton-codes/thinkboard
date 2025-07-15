855007b329c976fbfd1afd1ce6a7a69229c8cc37
Nitesh <nkj456789@gmail.com>
2025-07-15 17:11:35 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,14 +11,26 @@ dotenv.config();
 
 const app=express();
 const PORT = process.env.PORT || 5001
-
+const __dirname=path.resolve();
 
 //middleware used to obtain req.body and destructure it
-app.use(cors("http://localhost:5001"));
+if(process.env.NODE_ENV!=="production"){
+    app.use(cors({
+        origin:"http://localhost:5001"
+    }));
+}
 app.use(express.json());
 app.use(rateLimit);
 
 app.use("/api/notes",notesRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+};
 // app.use("/api/payments",paymentRoutes);
 // app.use("/api/post",postRoutes);
 
